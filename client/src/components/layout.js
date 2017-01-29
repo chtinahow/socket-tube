0894c32deeede5be12bc0d254a981005869102d8
jjurman <jjurman@constantcontact.com>
2017-01-29 18:41:35 -0500

    some style

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -3,15 +3,29 @@ const socketIO = require('./socketIO');
 const header = require('../elements/header');
 const footer = require('../elements/footer');
 
+const layoutStyle = `
+  margin: auto;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const pageStyle = `
+  height: 100%;
+`
 
 module.exports = (pageComponent, state, prev, send) => {
   const page = pageComponent.bind(this, state, prev, send);
   const socket = socketIO.bind(this, state, prev, send);
   return html`
-    <div>
+    <div style=${layoutStyle}>
       ${header()}
       ${socket()}
-      ${page()}
+      <div style=${pageStyle}>
+        ${page()}
+      </div>
       ${footer()}
     </div>
   `
